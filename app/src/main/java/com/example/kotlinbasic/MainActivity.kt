b71c366d857f64023ab32af07a35f88115a16011
elalimardani <elnaz.alimardani@gmail.com>
2020-11-03 14:13:35 -0500

    set background and change X and Y of screenshot programmatically in landscape

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -2,34 +2,36 @@ package com.example.kotlinbasic
 
 import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import org.jetbrains.anko.longToast
 
 
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         checkOsVersion()
 
 
-        val imageView: ImageView = findViewById(R.id.imageView)
         val btn: Button = findViewById(R.id.button)
+        val imageView: ImageView = findViewById(R.id.imageView)
 
         btn.setOnClickListener {
             val image: Bitmap = takeScreenshotOfRootView(imageView)
             imageView.setImageBitmap(image)
         }
-
-
     }
 
 
@@ -46,9 +48,16 @@ class MainActivity : AppCompatActivity() {
    override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         val textView = findViewById<View>(R.id.textView) as TextView
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val constraintlayout: ConstraintLayout = findViewById(R.id.main)
+
 
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             textView.setText(R.string.landscape)
+            imageView.x = -200F
+            imageView.y = 800F
+            //constraintlayout.setBackgroundResource()
+
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
             textView.setText(R.string.portrait)
 
